5a8107c1142e9e288a7888de36d0fc4ee900c264
sildater <silvandavidpeter@gmail.com>
2021-06-08 11:06:32 +0200

    improved highlighting

--- sketch.js
+++ sketch.js
@@ -10,6 +10,9 @@ let perf_lines = [];
 let pile_lines = [];
 let term_lines = [];
 let clicked_objects = [];
+let term_strings = [];
+
+let default_settings;
 
 function preload() {
   term_table = loadTable("data/term_data.csv", 'csv', 'header');
@@ -18,16 +21,36 @@ function preload() {
 }
 
 function setup() {
-  let canvas = createCanvas(1500, 800);
+  default_settings = {
+    perf_line:color(20,140),
+    perf_background:color(20, 50),
+    //pile_line:color(183, 172, 68, 230),
+    //term_line: color(255,170,0,230),
+    background: color(255,255,255,255),
+    
+    group1_from:color(23, 244, 120, 100),
+    group1_to:color(23, 23, 244, 100),
+    group2_from:color(218, 0, 32, 150),
+    group2_to:color(72, 218, 9, 150),
+  
+    height:800,
+    width:1500,
+    font_size: 10,
+
+  };
+  default_settings.width = max(1450, windowWidth);
+  let max_height = default_settings.width - 900;
+  default_settings.height = min(max(800, windowHeight),max_height);
+  default_settings.font_size = default_settings.height /45 /5 *3;
+  let canvas = createCanvas(default_settings.width, default_settings.height );
   canvas.mousePressed(checkclick);
   create_objects();
   noLoop();
-  noStroke()
-
+  noStroke();
 }
 
 function draw() {
-  background(255);
+  background(default_settings.background);
   
   for (var key in term_objects) {
     term_objects[key].display();
@@ -44,10 +67,18 @@ function draw() {
   for (var key in perf_lines) {
     perf_lines[key].display();
       }
-
   for (var key in term_lines) {
     term_lines[key].display();
       }
+
+  push();
+  textSize(10);
+  
+  for (var key in term_strings) {
+    text(term_strings[key], 230, default_settings.height/2-(term_strings.length/2-1)*12+key*12, default_settings.height*4, default_settings.height/4);
+      }
+  
+  pop();
 }
 
 
@@ -97,45 +128,53 @@ function parse_perf_row(r){
 
 
 function create_pile_lines_from_pile(pile_id, term_idx){
-  pile_lines=pile_lines.concat(term_idx.map((term_id)=>{return new Connection(pile_objects[pile_id].x_conn,
-                                                              pile_objects[pile_id].y_conn,
-                                                              term_objects[term_id].x,
-                                                              term_objects[term_id].y,
-                                                              pile_objects[pile_id].color)}))
+  pile_lines=pile_lines.concat(term_idx.map((term_id)=>{term_objects[term_id].clicked = true;
+    return new Connection(pile_objects[pile_id].x_conn,
+                          pile_objects[pile_id].y_conn,
+                          term_objects[term_id].x,
+                          term_objects[term_id].y,
+                          pile_objects[pile_id].color)}))
 }
 
 function create_pile_lines_from_term(pile_idx, term_id){
-  pile_lines=pile_lines.concat(pile_idx.map((pile_id)=>{return new Connection(pile_objects[pile_id].x_conn,
-                                                              pile_objects[pile_id].y_conn,
-                                                              term_objects[term_id].x,
-                                                              term_objects[term_id].y,
-                                                              pile_objects[pile_id].color)}))
+  pile_lines=pile_lines.concat(pile_idx.map((pile_id)=>{pile_objects[pile_id].clicked = true;
+    return new Connection(pile_objects[pile_id].x_conn,
+                          pile_objects[pile_id].y_conn,
+                          term_objects[term_id].x,
+                          term_objects[term_id].y,
+                          pile_objects[pile_id].color)}))
 }
 
 function create_perf_lines_from_term(term_id, perf_idx){
-  perf_lines=perf_lines.concat(perf_idx.map((perf_id)=>{return new Connection(perf_objects[perf_id].x_conn,
-                                                              perf_objects[perf_id].y_conn,
-                                                              term_objects[term_id].x,
-                                                              term_objects[term_id].y,
-                                                              color(20,140))}))
-}
+  perf_lines=perf_lines.concat(perf_idx.map((perf_id)=>{perf_objects[perf_id].clicked = true;
+    return new Connection(perf_objects[perf_id].x_conn,
+                          perf_objects[perf_id].y_conn,
+                          term_objects[term_id].x,
+                          term_objects[term_id].y,
+                          default_settings.perf_line)}))
+    }
 
 function create_perf_lines_from_perf(term_idx, perf_id){
-  perf_lines=perf_lines.concat(term_idx.map((term_id)=>{return new Connection(perf_objects[perf_id].x_conn,
-                                                              perf_objects[perf_id].y_conn,
-                                                              term_objects[term_id].x,
-                                                              term_objects[term_id].y,
-                                                              color(20,140))}))
+  perf_lines=perf_lines.concat(term_idx.map((term_id)=>{term_objects[term_id].clicked = true;
+    return new Connection(perf_objects[perf_id].x_conn,
+                          perf_objects[perf_id].y_conn,
+                          term_objects[term_id].x,
+                          term_objects[term_id].y,
+                          default_settings.perf_line)}))
 }
-
 
 function create_term_lines_from_term(term_id_orig, term_idx, col){
-  term_lines= term_lines.concat(term_idx.map((term_id)=>{return new Connection(term_objects[term_id_orig].x,
-                                                              term_objects[term_id_orig].y,
-                                                              term_objects[term_id].x,
-                                                              term_objects[term_id].y,
-                                                              col)}))
+  term_lines= term_lines.concat(term_idx.map((term_id)=>{ term_objects[term_id].clicked = true;
+    return new Connection(term_objects[term_id_orig].x,
+                          term_objects[term_id_orig].y,
+                          term_objects[term_id].x,
+                          term_objects[term_id].y,
+                          col)}))
 }
+
+
+
+
 
 
 
@@ -151,7 +190,7 @@ class Connection {
     push();
     fill(this.color);
     stroke(this.color);
-    strokeWeight(2)
+    strokeWeight(1.5);
     line(this.x1, this.y1, this.x2, this.y2);
     pop();
   }
@@ -162,44 +201,59 @@ class Connection {
 
 
 
-
-
-
 class Term {
   constructor(x,y,name, id, id_pile1, id_pile2, name_pile1, name_pile2, neighbors1, neighbors2, performances) {
-    this.x = x*400+600;
-    this.y = y*400+400;
-    this.name = name;
+    this.x = x*default_settings.height/2 + default_settings.height/2 + 400;
+    this.y = y*default_settings.height/2 +default_settings.height/2;
+    this.name = name.replace("_", " ");
     this.id = id;
     this.id_pile1 = id_pile1;
     this.id_pile2 = id_pile2;
     this.ids_pile = [id_pile1, id_pile2];
-    this.name_pile1 = name_pile1;
-    this.name_pile2 = name_pile2;
+    this.name_pile1 = name_pile1.replace("_", " ");
+    this.name_pile2 = name_pile2.replace("_", " ");
     this.neighbors1 = neighbors1;
     this.neighbors2 = neighbors2;
     this.perf_idx = performances;
+    this.clicked = false;
   }
 
   display() {
+    let diameter = 20;
+    let bcolor1 = pile_objects[this.id_pile1].color;
+    let bcolor2 = pile_objects[this.id_pile2].color;
+    let fontsize = default_settings.font_size;
+    if (this.clicked) {
+      diameter = 34;
+      bcolor1 = color(...bcolor1.levels.slice(0,3), 125);
+      bcolor2 = color(...bcolor2.levels.slice(0,3), 165);
+      fontsize = fontsize + 2;
+      term_strings.push(this.name.concat(" (pile 1: ", this.name_pile1, ", pile 2: ", this.name_pile2, ")"));
+    }
     push();
     //stroke(pile_objects[this.id_pile1].color);
-    fill(pile_objects[this.id_pile1].color);
-    circle(this.x, this.y, 20);
+    fill(bcolor1);
+    circle(this.x, this.y, diameter);
     pop();
     push();
     //stroke(pile_objects[this.id_pile2].color);
-    fill(pile_objects[this.id_pile2].color);
-    circle(this.x, this.y, 10);
+    fill(bcolor2);
+    circle(this.x, this.y, diameter/2);
     pop();
+
     //stroke(color(0));
+    push();
+    textSize(fontsize);
     text(this.name, this.x, this.y);
+    pop();
   }
 
   tryclick() {
+    this.clicked = false;
     if(Math.sqrt((mouseX-this.x)**2 + (mouseY-this.y)**2)<= 10){
   
      clicked_objects.push(this);
+     this.clicked = true;
     }
   }
   click() {
@@ -208,6 +262,8 @@ class Term {
     create_pile_lines_from_term(this.ids_pile, this.id);
     create_term_lines_from_term(this.id, this.neighbors1, pile_objects[this.id_pile1].color);
     create_term_lines_from_term(this.id, this.neighbors2, pile_objects[this.id_pile2].color);
+
+    
 
     }
 }
@@ -222,23 +278,24 @@ class Term {
 class Pile {
   constructor(name, id, group, within_group_id, term_idx) {
 
-    this.name = name;
+    this.name = name.replace("_", " ");
     this.id = id;
     this.group = group;
     this.within_group_id = within_group_id;
     this.term_idx = term_idx;
 
     this.x = 10;
-    this.yl = 800 /44;
+    this.yl = default_settings.height /44;
     this.y = this.compute_y();
     this.xl = 200;
-    this.x_conn = 210;
+    this.x_conn = 220;
     this.y_conn = this.y + 0.5*this.yl;
     
-
-    this.color = this.compute_color()
-
+    this.clicked = false;
+    // piles are colored according to gradient per group
+    this.color = this.compute_color();
   }
+
   compute_y () {
     let offset = 0;
     
@@ -250,58 +307,60 @@ class Pile {
   }
   compute_color() {
     if (this.group == 2) {
-      let from = color(218, 0, 32, 150);
-      let to = color(72, 218, 9, 150);
       colorMode(RGB); // Try changing to HSB.
-      return lerpColor(from, to, this.within_group_id/18)
+      return lerpColor(default_settings.group2_from, default_settings.group2_to, this.within_group_id/18)
     }
     else if (this.group == 1) {
-      let from = color(23, 244, 120, 100);
-      let to = color(23, 23, 244, 100);
-      colorMode(RGB); // Try changing to HSB.
-      return lerpColor(from, to, this.within_group_id/24)
-    }
 
+      colorMode(RGB); // Try changing to HSB.
+      return lerpColor(default_settings.group1_from, default_settings.group1_to, this.within_group_id/24)
+    }
   }
 
 
 
   display() {
-    push()
-    fill(this.color);
-    //circle(this.x, this.y, 10);
-    //stroke(this.color);
-    rect(this.x, this.y, this.xl, this.yl-1, 10);
-    //stroke(0,100)
-    //strokeWeight(2)
-    fill(0,200)
+    let x_extra = 0;
+    let bcolor = this.color;
+    let font_size = default_settings.font_size;
+    if (this.clicked) {
+      x_extra = 10;
+      bcolor = color(...bcolor.levels.slice(0,3), 225);
+      font_size = default_settings.font_size + 2;;
+    }
+
+    push();
+    fill(bcolor);
+    rect(this.x-x_extra, this.y, this.xl+2*x_extra, this.yl-1, 10);
+    pop();
+
+    push();
+    textSize(font_size);
     text(this.name, this.x+50, this.y+0.8*this.yl);
-    pop()
+    pop();
+  
   }
 
   tryclick() {
+    this.clicked = false;
     if(mouseX>=this.x && mouseX<this.x+this.xl && mouseY>=this.y && mouseY<this.y+this.yl){
      console.log("clicked pile", this.name);
      clicked_objects.push(this);
+     this.clicked = true;
     }
   }
   click() {
-    
      create_pile_lines_from_pile(this.id, this.term_idx);
-
-
      for(var i = 0; i < this.term_idx.length; i++){
-       
        let connected_term = terms_array[this.term_idx[i]];
        create_perf_lines_from_term(connected_term.id, connected_term.perf_idx);
      }
-
     }
-  
-
-
-
 }
+
+
+
+
 
 
 
@@ -310,20 +369,22 @@ class Pile {
 
 class Performance {
   constructor(name, music_id, within_id,term_idx) {
-
-    this.name = name;
+    let namesplit = name.split("_");
+    namesplit = namesplit.map((word)=>{return word.charAt(0).toUpperCase()+word.slice(1,word.length);})
+    this.name = namesplit.join(" ");
     this.music_id = music_id;
     this.within_id = within_id;
     this.term_idx = term_idx;
 
-    this.x = 1090;
-    this.yl = 800/45;
+    this.x = default_settings.height+400+50;//1090;
+    this.yl = default_settings.height/45;
     this.y = this.compute_y();
-    this.xl = 400;
-    this.x_conn = 1090;
+    this.xl = 440;
+    this.x_conn = this.x-10;
     this.y_conn = this.y + 0.5*this.yl;
 
-    this.color = color(20,230);
+    this.color = default_settings.perf_background;
+    this.clicked = false;
     
     
 
@@ -337,28 +398,37 @@ class Performance {
 
 
   display() {
-    push()
-    fill(20,50);
-    //circle(this.x, this.y, 10);
-    //stroke(this.color);
-    rect(this.x, this.y, this.xl, this.yl-1, 10);
-    //stroke(0,100)
-    //strokeWeight(2)
-    fill(this.color)
+    let x_extra = 0;
+    let bcolor = this.color;
+    let font_size = default_settings.font_size;
+    if (this.clicked) {
+      x_extra = 10;
+      bcolor = color(...bcolor.levels.slice(0,3), 115);
+      font_size = default_settings.font_size + 2;;
+    }
+
+
+    push();
+    fill(bcolor);
+    rect(this.x-x_extra, this.y, this.xl+2*x_extra, this.yl-1, 10);
+    pop();
+
+    push();
+    textSize(font_size);
     text(this.name, this.x+50, this.y+0.8*this.yl);
-    pop()
- 
+    pop();
   }
   tryclick() {
+    this.clicked = false;
     if(mouseX>=this.x && mouseX<this.x+this.xl && mouseY>=this.y && mouseY<this.y+this.yl){
      console.log("clicked performance", this.name);
      clicked_objects.push(this);
+     this.clicked = true;
     }
   }
   click() {
     create_perf_lines_from_perf(this.term_idx, this.music_id);
     for(var i = 0; i < this.term_idx.length; i++){
-      console.log(this.term_idx[i])
       let connected_term = terms_array[this.term_idx[i]];
       create_pile_lines_from_term(connected_term.ids_pile, connected_term.id);
     }
@@ -385,6 +455,7 @@ function checkclick() {
     pile_lines = [];
     term_lines = [];
     clicked_objects = [];
+    term_strings = [];
     for(var i = 0; i < piles_array.length; i++){
       piles_array[i].tryclick();
     }
@@ -396,8 +467,11 @@ function checkclick() {
     }
     for(var i = 0; i < clicked_objects.length; i++){
       clicked_objects[i].click();
+
     }
     redraw();
+
+  
   
   }
   else if (mouseButton === RIGHT) {
